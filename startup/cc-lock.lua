9f63ed060756e6aa0e9da68617e018e1394efebe
ascpial <charles@ascpial.fr>
2022-04-10 21:21:45 +0200

    better startup

--- startup/cc-lock.lua
+++ startup/cc-lock.lua
@@ -1,27 +1,26 @@
 local function main()
     local pullEvent_old = os.pullEvent
     os.pullEvent = os.pullEventRaw
+    
+    local auth_manager = require("auth")
 
-    if not fs.exists("credentials") then
-        print("Use the register command to lock the computer.")
+    local authWindow = auth_manager:new()
+    
+    authWindow:start()
+    
+    if not authWindow.authentificated then
+        os.shutdown()
     else
-        local auth_manager = require("auth")
-
-        local authWindow = auth_manager:new()
-        
-        authWindow:start()
-        
-        if not authWindow.authentificated then
-            os.shutdown()
-        else
-            print("Welcome!")
-            os.pullEvent = pullEvent_old
-        end
+        print("Welcome!")
+        os.pullEvent = pullEvent_old
     end
 end
 
-shell.setDir("/")
-package.path = package.path .. ";/cc-lock/?;/cc-lock/?.lua;/?;/?.lua;/?/init.lua"
-local shield = require("shield")
-shield(main)
-shell.setDir("/startup")
+if not fs.exists("credentials") then
+    print("Use the register command to lock the computer.")
+else
+    shell.setDir("/")
+    package.path = "/cc-lock/?;/cc-lock/?.lua;/?;/?.lua;/?/init.lua;" .. package.path
+    local shield = require("shield")
+    shield(main)
+end
